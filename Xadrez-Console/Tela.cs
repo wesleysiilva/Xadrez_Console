7ec8fc4f01986ec1aa2b6b3d3a9d661710193d45
Wesley Silva <wesleysa95@gmail.com>
2019-05-14 22:54:22 -0300

    Testes iniciais com movimentos possiveis das pecas

--- Xadrez-Console/Tela.cs
+++ Xadrez-Console/Tela.cs
@@ -9,18 +9,36 @@ namespace Xadrez_Console {
             Console.Write(8 - i + " ");
 
             for (int j = 0; j < tab.colunas; j++) {
-
-               if (tab.peca(i, j) == null) {
-                  Console.Write("- ");
-               }
-               else {
-                  imprimirPeca(tab.peca(i, j));
-                  Console.Write(' ');
-               }
+               imprimirPeca(tab.peca(i, j));
+               Console.Write(' ');
             }
             Console.WriteLine();
          }
          Console.WriteLine("  a b c d e f g h");
+      }
+
+      public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis) {
+         ConsoleColor fundoOriginal = Console.BackgroundColor;
+         ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
+
+         for (int i = 0; i < tab.linhas; i++) {
+            Console.Write(8 - i + " ");
+
+            for (int j = 0; j < tab.colunas; j++) {
+
+               if (posicoesPossiveis[i, j]) {
+                  Console.BackgroundColor = fundoAlterado;
+               }
+               else {
+                  Console.BackgroundColor = fundoOriginal;
+               }
+               imprimirPeca(tab.peca(i, j));
+               Console.BackgroundColor = fundoOriginal;
+            }
+            Console.WriteLine();
+         }
+         Console.WriteLine("  a b c d e f g h");
+         Console.BackgroundColor = fundoOriginal;
       }
 
       public static PosicaoXadrez lerPosicaoXadrez() {
@@ -31,16 +49,21 @@ namespace Xadrez_Console {
       }
 
       public static void imprimirPeca(Peca peca) {
-         if (peca.cor == Cor.Branca) {
-            Console.Write(peca);
+         if (peca == null) {
+            Console.Write("- ");
          }
          else {
-            ConsoleColor aux = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write(peca);
-            Console.ForegroundColor = aux;
+            if (peca.cor == Cor.Branca) {
+               Console.Write(peca);
+            }
+            else {
+               ConsoleColor aux = Console.ForegroundColor;
+               Console.ForegroundColor = ConsoleColor.Yellow;
+               Console.Write(peca);
+               Console.ForegroundColor = aux;
+            }
+            Console.Write(' ');
          }
-
       }
    }
 }
--- Xadrez-Console/Xadrez/Torre.cs
+++ Xadrez-Console/Xadrez/Torre.cs
@@ -8,5 +8,57 @@ namespace xadrez {
       public override string ToString() {
          return "T";
       }
+
+      private bool podeMover(Posicao pos) {
+         Peca p = tab.peca(pos);
+         return p == null || p.cor != cor;
+      }
+
+      public override bool[,] movimentosPossiveis() {
+         bool[,] mat = new bool[tab.linhas, tab.colunas];
+         Posicao pos = new Posicao(0, 0);
+
+         //norte
+         pos.definirValores(posicao.Linha - 1, posicao.Coluna); 
+         while (tab.posicaoValida(pos) && podeMover(pos)) {
+            mat[pos.Linha, pos.Coluna] = true;
+            if (tab.peca(pos) != null && tab.peca(pos).cor != cor) {
+               break; 
+            }
+            pos.Linha = pos.Linha - 1;
+         }
+
+         //sul
+         pos.definirValores(posicao.Linha + 1, posicao.Coluna);
+         while (tab.posicaoValida(pos) && podeMover(pos)) {
+            mat[pos.Linha, pos.Coluna] = true;
+            if (tab.peca(pos) != null && tab.peca(pos).cor != cor) {
+               break;
+            }
+            pos.Linha = pos.Linha + 1;
+         }
+         
+         //leste
+         pos.definirValores(posicao.Linha , posicao.Coluna +1);
+         while (tab.posicaoValida(pos) && podeMover(pos)) {
+            mat[pos.Linha, pos.Coluna] = true;
+            if (tab.peca(pos) != null && tab.peca(pos).cor != cor) {
+               break;
+            }
+            pos.Coluna = pos.Coluna + 1;
+         }
+
+         //oeste
+         pos.definirValores(posicao.Linha, posicao.Coluna - 1);
+         while (tab.posicaoValida(pos) && podeMover(pos)) {
+            mat[pos.Linha, pos.Coluna] = true;
+            if (tab.peca(pos) != null && tab.peca(pos).cor != cor) {
+               break;
+            }
+            pos.Coluna = pos.Coluna - 1;
+         }
+
+         return mat;
+      }
    }
 }
